8843edc53b09f226c343c17d45906c87a23e7b6d
Seah Siang Hwee <sianghwee@Seahs-MacBook-Pro.local>
2022-05-08 18:53:21 +0800

    added functionality to slider. slider only works on clicks and not drag, slider does not delete previous grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,5 @@
 var gridcontainer = document.querySelector(".gridcontainer");
 
-// function to create button for users to input values
-/* const inputContainer = document.querySelector(".inputcontainer");
-const inputButton = document.createElement("button");
-inputButton.classList.add("inputbutton");
-inputButton.textContent = "Click here to input number of rows and columns.";
-inputContainer.appendChild(inputButton); */
-
 // display values on slider
 var slider = document.getElementById("range");
 var output = document.querySelector(".value");
@@ -22,46 +15,25 @@ let clearGrid = () => {
     gridcontainer.querySelectorAll('*').forEach(n => n.remove());
 }
 
-// function to find userinput
-/*let rows;
-let rowWidth = 100/rows;
-
-let userInput = () => {
-    rows = window.prompt("Please insert number of rows/columns (up to 100).");
-    if (rows > 100){
-        userInput();
-    }
-    else if(rows == ""){
-        userInput();
-    }
-    else {
-        rowWidth = 100/rows;
-        console.log(rowWidth);
-    }
-} */
-
 // function to create grid
-let i,j;
-let createGrid = () => {
+
+slider.addEventListener("click", gridSizer);
+
+function gridSizer(e) {
+    clearGrid;
+    let i,j;
+    var rows = slider.value;
+    width = 100/rows;
     for (i=1; i<=rows; i++){
         for (j=1; j<=rows; j++){
             const gridboxes = document.createElement("div");
             gridboxes.classList.add("gridbox");
-            gridboxes.style.width = rowWidth + "%";
-            gridboxes.style.paddingBottom= rowWidth + "%";
+            gridboxes.style.width = width + "%";
+            gridboxes.style.paddingBottom= width + "%";
             gridcontainer.appendChild(gridboxes);
             }
         }
-    }
-
-// event listener for inputButton
-inputButton.addEventListener("click", function(){
-    clearGrid();
-    userInput();
-    createGrid();
-    colourGrid();
-});
-
+}
 // function to colour gridboxes red after cursor moves over
 let colourGrid = () => {
     const gridboxes = document.querySelectorAll(".gridbox");
