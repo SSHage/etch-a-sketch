8dcb4664a700a0fda96135cfc40478fb0315f6cc
Seah Siang Hwee <sianghwee@Seahs-MacBook-Pro.local>
2022-05-08 18:34:38 +0800

    added user text input for number of rows/columns

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
 var gridcontainer = document.querySelector(".gridcontainer");
 
 // function to create button for users to input values
-const inputContainer = document.querySelector(".inputcontainer");
+/* const inputContainer = document.querySelector(".inputcontainer");
 const inputButton = document.createElement("button");
 inputButton.classList.add("inputbutton");
 inputButton.textContent = "Click here to input number of rows and columns.";
-inputContainer.appendChild(inputButton);
+inputContainer.appendChild(inputButton); */
+
+// display values on slider
+var slider = document.getElementById("range");
+var output = document.querySelector(".value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 // function to clear grid
 let clearGrid = () => {
@@ -13,35 +23,32 @@ let clearGrid = () => {
 }
 
 // function to find userinput
-let rows;
-let columns;
+/*let rows;
 let rowWidth = 100/rows;
-let columnWidth = 100/columns;
 
 let userInput = () => {
-    rows = window.prompt("Please insert number of rows (up to 100).");
-    columns = window.prompt("Please insert number of columns (up to 100).");
-    if (rows > 100 || columns > 100){
+    rows = window.prompt("Please insert number of rows/columns (up to 100).");
+    if (rows > 100){
         userInput();
     }
-    else if(rows == "" || columns == ""){
+    else if(rows == ""){
         userInput();
     }
     else {
         rowWidth = 100/rows;
-        columnWidth = 100/columns;
+        console.log(rowWidth);
     }
-}
+} */
 
 // function to create grid
 let i,j;
 let createGrid = () => {
     for (i=1; i<=rows; i++){
-        for (j=1; j<=columns; j++){
+        for (j=1; j<=rows; j++){
             const gridboxes = document.createElement("div");
             gridboxes.classList.add("gridbox");
-            gridboxes.style.width = columnWidth + "%";
-            gridboxes.style.paddingBottom= columnWidth + "%";
+            gridboxes.style.width = rowWidth + "%";
+            gridboxes.style.paddingBottom= rowWidth + "%";
             gridcontainer.appendChild(gridboxes);
             }
         }
@@ -58,26 +65,19 @@ inputButton.addEventListener("click", function(){
 // function to colour gridboxes red after cursor moves over
 let colourGrid = () => {
     const gridboxes = document.querySelectorAll(".gridbox");
-    gridboxes.forEach(gridbox => 
+    /* gridboxes.forEach(gridbox => 
         gridbox.addEventListener("mouseover", () =>
             gridbox.classList.add("red")
                 )
-            );
+            ); */
+        
     // alternative function
-    /* for (const gridbox of gridboxes) {
+    for (const gridbox of gridboxes) {
+        let opacityval = 0;
         gridbox.addEventListener("mouseover", function(e){
             this.classList.add("red");
+            opacityval += 0.2;
+            this.style.opacity = opacityval;
         })
-    } */
+    } 
 }
-
-
-
-
-
-
-
-
-
-
-
